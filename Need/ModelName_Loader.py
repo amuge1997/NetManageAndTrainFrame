7cca5f1a0eff33e5be15c480c3696dd1b0940ca0
Amuge <amuge1997z@qq.com>
2020-04-23 16:40:29 +0800

    修改,小幅修改与修正bug;

--- Need/ModelName_Loader.py
+++ Need/ModelName_Loader.py
@@ -3,9 +3,8 @@
 
 class Loader:
     def __init__(self):
-        import os
         # Loader 所在目录
-        self.sr_now_dir = os.path.split(os.path.split(__file__)[0])[1] + '/'
+        self.sr_now_dir = './Need'
 
     def get_loader(self):
         loader = None
--- Need/ModelName_Model.py
+++ Need/ModelName_Model.py
@@ -1,7 +1,7 @@
 import torch.nn as nn
 
 
-class Net(nn.Module):  # 定义模型
+class Model(nn.Module):  # 定义模型
     def __init__(self):
         nn.Module.__init__(self)
 
